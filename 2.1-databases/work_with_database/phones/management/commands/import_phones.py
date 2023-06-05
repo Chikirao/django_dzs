754f2eff5f1c04f09311f33b3338b51ff32b1349
Chikirao <ivankonanch@gmail.com>
2023-06-05 10:06:02 +0300

    2.1 compleate

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -1,17 +1,30 @@
 import csv
+from datetime import datetime
 
 from django.core.management.base import BaseCommand
 from phones.models import Phone
 
 
 class Command(BaseCommand):
-    def add_arguments(self, parser):
-        pass
+    help = "Import phones from csv file"
 
     def handle(self, *args, **options):
-        with open('phones.csv', 'r') as file:
-            phones = list(csv.DictReader(file, delimiter=';'))
+        with open("phones.csv", "r") as csvfile:
+            phone_reader = csv.reader(csvfile, delimiter=";")
+            next(phone_reader)
 
-        for phone in phones:
-            # TODO: Добавьте сохранение модели
-            pass
+            for row in phone_reader:
+                print(row)
+                id, name, image, price, release_date_str, lte_exists_str = row
+                release_date = datetime.strptime(release_date_str, "%Y-%m-%d").date()
+                lte_exists = lte_exists_str.lower() == "true"
+                phone = Phone(
+                    id=id,
+                    name=name,
+                    image=image,
+                    price=price,
+                    release_date=release_date,
+                    lte_exists=lte_exists,
+                )
+                phone.save()
+        self.stdout.write(self.style.SUCCESS("Phones have been imported successfully!"))
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,17 +1,31 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+
+from phones.models import Phone
 
 
 def index(request):
-    return redirect('catalog')
+    return redirect("catalog")
 
 
 def show_catalog(request):
-    template = 'catalog.html'
-    context = {}
+    phones = Phone.objects.all()
+
+    # Определяем параметры сортировки из GET запроса
+    sort_by = request.GET.get("sort")
+    if sort_by == "name":
+        phones = phones.order_by("name")
+    elif sort_by == "max_price":
+        phones = phones.order_by("-price")
+    elif sort_by == "min_price":
+        phones = phones.order_by("price")
+
+    context = {"phones": phones}
+    template = "catalog.html"
     return render(request, template, context)
 
 
 def show_product(request, slug):
-    template = 'product.html'
-    context = {}
+    phone = get_object_or_404(Phone, slug=slug)
+    template = "product.html"
+    context = {"phone": phone}
     return render(request, template, context)
